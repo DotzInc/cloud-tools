efa02fa79cfa40d31818d8f83287c9ea956dde25
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-13 12:49:21 -0300

    feat: pass initialization arguments for sdks clients

--- cloud/google/storage.py
+++ cloud/google/storage.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 from google.cloud import storage
 
 
 class Uploader:
-    def __init__(self):
-        self.client = storage.Client()
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.client = storage.Client(*args, **kwargs)
 
     def upload(self, bucket_name: str, destination_filename: str, source_filename: str) -> None:
         bucket = self.client.bucket(bucket_name)
