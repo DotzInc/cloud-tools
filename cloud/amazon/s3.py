ffe8d101d19bf13c79263f7d10e84c79afe7bcad
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-18 19:25:42 -0300

    feat: implement the storage downloader protocol for aws s3 service
    * Add implementation of the StorageDownloader protocol for Amazon S3 service.

--- cloud/amazon/s3.py
+++ cloud/amazon/s3.py
@@ -9,3 +9,11 @@ class Uploader:
 
     def upload(self, bucket_name: str, destination_filename: str, source_filename: str) -> None:
         self.client.upload_file(source_filename, bucket_name, destination_filename)
+
+
+class Downloader:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.client = boto3.client("s3", *args, **kwargs)
+
+    def download(self, bucket_name: str, source_filename: str, destination_filename: str) -> None:
+        self.client.download_file(bucket_name, source_filename, destination_filename)
--- tests/amazon/test_s3.py
+++ tests/amazon/test_s3.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest import mock
 
-from cloud.amazon.s3 import Uploader
-from cloud.protocols import StorageUploader
+from cloud.amazon.s3 import Downloader, Uploader
+from cloud.protocols import StorageDownloader, StorageUploader
 
 
 class TestStorageUploader(unittest.TestCase):
@@ -21,3 +21,21 @@ class TestStorageUploader(unittest.TestCase):
 
         cli = client_mock.return_value
         cli.upload_file.assert_called_once_with(source_path, bucket_name, object_name)
+
+
+class TestStorageDownloader(unittest.TestCase):
+    @mock.patch("boto3.client")
+    def test_storage_download(self, client_mock):
+        downloader = Downloader()
+
+        self.assertIsInstance(downloader, StorageDownloader)
+        client_mock.assert_called_once_with("s3")
+
+        bucket_name = "test-bucket"
+        object_name = "test.txt"
+        destination = "/tmp/test.txt"
+
+        downloader.download(bucket_name, object_name, destination)
+
+        cli = client_mock.return_value
+        cli.download_file.assert_called_once_with(bucket_name, object_name, destination)
