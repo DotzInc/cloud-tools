5f191745e39538b6e2b755456453c0fd61cf91dd
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-18 20:05:50 -0300

    refactor: reduce repetion on google cloud storage module
    * Update module by creating a base client class implementing boath
      StorageDownloader and StorageUploader protocols then change the
      classes Uploader and Downloader to become sub-classes of Client.

--- cloud/google/storage.py
+++ cloud/google/storage.py
@@ -3,21 +3,22 @@ from typing import Any
 from google.cloud import storage
 
 
-class Uploader:
+class Client:
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.client = storage.Client(*args, **kwargs)
 
     def upload(self, bucket_name: str, destination_filename: str, source_filename: str) -> None:
-        bucket = self.client.bucket(bucket_name)
-        blob = bucket.blob(destination_filename)
+        blob = self.client.bucket(bucket_name).blob(destination_filename)
         blob.upload_from_filename(source_filename)
 
-
-class Downloader:
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.client = storage.Client(*args, **kwargs)
-
     def download(self, bucket_name: str, source_filename: str, destination_filename: str) -> None:
-        bucket = self.client.bucket(bucket_name)
-        blob = bucket.blob(source_filename)
+        blob = self.client.bucket(bucket_name).blob(source_filename)
         blob.download_to_filename(destination_filename)
+
+
+class Uploader(Client):
+    pass
+
+
+class Downloader(Client):
+    pass
