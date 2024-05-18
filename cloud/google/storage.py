398c442934724ba0807a85b7e2e2a8270356fc5a
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-18 19:52:31 -0300

    feat: implement the storage downloader protocol for cloud storage service
    * Add implementation of the StorageDownloader protocol for Google Cloud Storage service.

--- cloud/google/storage.py
+++ cloud/google/storage.py
@@ -11,3 +11,13 @@ class Uploader:
         bucket = self.client.bucket(bucket_name)
         blob = bucket.blob(destination_filename)
         blob.upload_from_filename(source_filename)
+
+
+class Downloader:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.client = storage.Client(*args, **kwargs)
+
+    def download(self, bucket_name: str, source_filename: str, destination_filename: str) -> None:
+        bucket = self.client.bucket(bucket_name)
+        blob = bucket.blob(source_filename)
+        blob.download_to_filename(destination_filename)
--- tests/google/test_storage.py
+++ tests/google/test_storage.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest import mock
 
-from cloud.google.storage import Uploader
-from cloud.protocols import StorageUploader
+from cloud.google.storage import Downloader, Uploader
+from cloud.protocols import StorageDownloader, StorageUploader
 
 
 class TestStorageUploader(unittest.TestCase):
@@ -27,3 +27,27 @@ class TestStorageUploader(unittest.TestCase):
 
         blob = bucket.blob.return_value
         blob.upload_from_filename.assert_called_once_with(source_path)
+
+
+class TestStorageDownloader(unittest.TestCase):
+    @mock.patch("google.cloud.storage.Client")
+    def test_storage_downloader(self, client_mock):
+        downloader = Downloader()
+
+        self.assertIsInstance(downloader, StorageDownloader)
+        client_mock.assert_called_once_with()
+
+        bucket_name = "test-bucket"
+        object_name = "test.txt"
+        destination = "/tmp/test.txt"
+
+        downloader.download(bucket_name, object_name, destination)
+
+        cli = client_mock.return_value
+        cli.bucket.assert_called_once_with(bucket_name)
+
+        bucket = cli.bucket.return_value
+        bucket.blob.assert_called_once_with(object_name)
+
+        blob = bucket.blob.return_value
+        blob.download_to_filename.assert_called_once_with(destination)
